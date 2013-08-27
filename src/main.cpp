80fed1d5537ff8c9e9c32206cae659b26ef83581
FuzzyBearBTC <peter@illustrate-archaeology.com>
2013-08-27 20:01:07 +0100

    added back in block halving

--- src/main.cpp
+++ src/main.cpp
@@ -830,8 +830,8 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
 {
     int64 nSubsidy = 1 * COIN;
 
-    // Subsidy is cut in half every 4 years
-  //  nSubsidy >>= (nHeight / 210000);
+    // Subsidy is cut in half every # blocks
+    nSubsidy >>= (nHeight / 210000);
 
     return nSubsidy + nFees;
 }
